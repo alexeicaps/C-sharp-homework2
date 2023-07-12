177f51bd47a928b2fd63ebe54498f24c7231118b
Alexei Capsamun <caps.alex@mail.ru>
2023-07-12 13:10:09 +0300

    update task15

--- task15/Program.cs
+++ task15/Program.cs
@@ -35,38 +35,3 @@ int checkTheDayOfWeek(int day)
         return 3;
     }
 }
-
-
-// int checkTheDayOfWeek(int day)
-// {
-//     if (day >= 1 && day <= 5)
-//     {
-//         Console.WriteLine("It's not a weekend day" + "No");
-//     }
-//     else if (day == 6 || day == 7)
-//     {
-//         Console.WriteLine("This is a weekend day" + "Yes");
-//     }
-//     else
-//     {
-//         Console.WriteLine("Такого дня нет!");
-//     }
-// }
-
-
-// Console.WriteLine("Задача 15 ");
-
-// Console.Write("Введи цифру, обозначающую день недели: ");
-// int dayNumber = Convert.ToInt32(Console.ReadLine());
-
-// void CheckingTheDayOfTheWeek (int dayNumber) {
-//   if (dayNumber == 6 || dayNumber == 7) {
-//   Console.WriteLine("(этот день выходной) -> да");
-//   }
-//   else if (dayNumber < 1 || dayNumber > 7) {
-//     Console.WriteLine("это вообще не день недели");
-//   }
-//   else Console.WriteLine("(этот день не выходной) -> нет");
-// }
-
-// CheckingTheDayOfTheWeek(dayNumber);
